b5ec5406a24092af89c4413d9dfba952df0ccfc1
narramuni <nmspavan@gmail.com>
2023-10-17 22:13:56 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] students = new string[5];
+        students[0] = "praveen";
+        students[1] = "kumar";
+        students[2] = "pranith";
+        students[3] = "kiran";
+        students[4] = "srinu";
+
+
+        int n = students.Length;
+        for (int i = 1; i < n; i++)
+        {
+            string currentStudent = students[i];
+            int j = i - 1;
+
+            while (j >= 0 && string.Compare(students[j], currentStudent) > 0)
+            {
+                students[j + 1] = students[j];
+                j--;
+            }
+
+            students[j + 1] = currentStudent;
+        }
+
+        Console.WriteLine("Sorted List of Students:");
+        foreach (var student in students)
+        {
+            Console.WriteLine($"Name: {student}");
+            Console.ReadLine();
+        }
+    }
+}
+
